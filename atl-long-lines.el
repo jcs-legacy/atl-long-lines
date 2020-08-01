a6a144fb1f045e775d4c39eb8d9de8d88805d15f
jenchieh <jcs090218@gmail.com>
2020-08-01 15:37:05 +0800

    Confirm first version.

--- atl-long-lines.el
+++ atl-long-lines.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Automatically truncate lines for long lines.
 ;; Keyword: truncate lines auto long
-;; Version: 0.0.1
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24.3"))
 ;; URL: https://github.com/jcs090218/atl-long-lines
 
